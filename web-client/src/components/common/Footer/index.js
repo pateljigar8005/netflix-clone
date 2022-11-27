7ebcedf36d097b16a6901ac96b8c9d8c4a9d2fda
Jigar Patel <patel.jigar8005@gmail.com>
2022-11-27 10:32:32 +0530

    netflix clone

---
+++ web-client/src/components/common/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./index.scss";
+
+const Footer = () => (
+  <footer className="footer-wrapper">
+    <a
+      className="footer-item footer-item--link"
+      href="https://github.com/pateljigar8005/Netflix-Clone"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Github
+    </a>
+    <p className="footer-item">&#169;2022 Jigar Patel</p>
+  </footer>
+);
+
+export default Footer;
